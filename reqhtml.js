abd116336201a631ff602384ba53dadbb78e9d1c
abhigna <abhignakanduri48@gmail.com>
2023-07-08 22:13:29 +0530

    deleted files

--- reqhtml.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function HTMLFile() {
-  return (
-    <iframe src="/path/to/your/html/request.html" width="100%" height="500px"></iframe>
-  );
-}
-
-export default HTMLFile;
